96d363421220cb4dbb237ec4ba9aeace4ff5cb00
azh <2550364626@qq.com>
2022-09-14 22:41:37 +0800

    注解识别

--- Spring_St/spring-framework-5.2.x/spring-azh-example/src/main/java/com/azh/springpostprocessor/SpringFactoryBeanApplication.java
+++ Spring_St/spring-framework-5.2.x/spring-azh-example/src/main/java/com/azh/springpostprocessor/SpringFactoryBeanApplication.java
@@ -1,10 +1,15 @@
 package com.azh.springpostprocessor;
 
 import com.azh.springpostprocessor.service.MySpringFactoryBeanContext;
+import com.azh.springpostprocessor.service.SpringComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringFactoryBeanApplication extends ClassPathXmlApplicationContext {
 	public static void main(String[] args) {
 		MySpringFactoryBeanContext mySpringFactoryBeanContext = new MySpringFactoryBeanContext("spring-beanFactoryPostProcessor.xml");
+
+		// 测试在 XML 使用 component-scan 是否可以扫描到注解类，debug 解析流程
+		SpringComponentScan springComponentScan = (SpringComponentScan) mySpringFactoryBeanContext.getBean("springComponentScan");
+		springComponentScan.testComponentScan();
 	}
 }
--- Spring_St/spring-framework-5.2.x/spring-azh-example/src/main/java/com/azh/springpostprocessor/service/SpringComponentScan.java
+++ Spring_St/spring-framework-5.2.x/spring-azh-example/src/main/java/com/azh/springpostprocessor/service/SpringComponentScan.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringComponentScan {
+	// 测试使用 component-scan 标签是否可以可以识别注解，并将注解类注入到 Spring IOC 容器中
+	public void testComponentScan(){
+		System.out.println("已经扫描到注解类，并使调用相应的方法输出");
+	}
 }
