03f88e6619904c814f83cf077d8d988dba01bbe2
azh <2550364626@qq.com>
2022-10-02 22:58:44 +0800

    注解-half

--- Spring_St/spring-framework-5.2.x/spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassPostProcessor.java
+++ Spring_St/spring-framework-5.2.x/spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassPostProcessor.java
@@ -66,6 +66,11 @@ import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 
 /**
+ * 此类是一个后置处理器的类，主要功能是参与 BeanFactory 的构造，主要功能如下：
+ * 	1. 解析加了 @Configuration 的配置类
+ * 	2. 解析 @ComponentScan 扫描的包
+ * 	3. 解析 @ComponentScans 扫描的包
+ * 	4. 解析 @Import 注解
  * {@link BeanFactoryPostProcessor} used for bootstrapping processing of
  * {@link Configuration @Configuration} classes.
  *
