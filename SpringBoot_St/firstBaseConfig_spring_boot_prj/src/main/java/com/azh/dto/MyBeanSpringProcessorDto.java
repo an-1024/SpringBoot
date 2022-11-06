c3dbe305fec8f952c17485331a87e399d2672078
azh <2550364626@qq.com>
2022-11-06 19:23:24 +0800

    commit

--- SpringBoot_St/firstBaseConfig_spring_boot_prj/src/main/java/com/azh/dto/MyBeanSpringProcessorDto.java
+++ SpringBoot_St/firstBaseConfig_spring_boot_prj/src/main/java/com/azh/dto/MyBeanSpringProcessorDto.java
@@ -10,26 +10,26 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Lazy;
 
 @Slf4j
-public class MyBeanSpringProcessorDto /*implements BeanPostProcessor*/ {
+public class MyBeanSpringProcessorDto implements BeanPostProcessor {
 
 
-//	@Override
-//	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-//		if((bean instanceof UserAnnotationDto) || (bean instanceof CarAnnotationDto)) {
-//			log.info("before----------------------{}", JSON.toJSONString(bean));
-//			log.info("before----------------------{}", beanName);
-//		}
-//		return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
-//	}
-//
-//	@Override
-//	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-//		if((bean instanceof UserAnnotationDto) || (bean instanceof CarAnnotationDto)) {
-//			log.info("after----------------------{}", JSON.toJSONString(bean));
-//			log.info("after----------------------{}", beanName);
-//		}
-//		return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
-//	}
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		if((bean instanceof UserAnnotationDto) || (bean instanceof CarAnnotationDto)) {
+			log.info("before----------------------{}", JSON.toJSONString(bean));
+			log.info("before----------------------{}", beanName);
+		}
+		return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		if((bean instanceof UserAnnotationDto) || (bean instanceof CarAnnotationDto)) {
+			log.info("after----------------------{}", JSON.toJSONString(bean));
+			log.info("after----------------------{}", beanName);
+		}
+		return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+	}
 
 	public void aspectAop(){
 		log.info("MyBeanSpringProcessorDto log");
