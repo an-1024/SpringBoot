e0d266f996c8dde61aaf264432e4200ea882c6de
azh <2550364626@qq.com>
2023-08-08 11:20:33 +0800

    SpringBootConfigFile

--- SpringBoot_St/springboot_prj/springboot_configfile_st/src/main/java/com/azh/controller/BookController.java
+++ SpringBoot_St/springboot_prj/springboot_configfile_st/src/main/java/com/azh/controller/BookController.java
@@ -1,19 +1,31 @@
 package com.azh.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.azh.config.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 
 @RestController
 public class BookController {
     @Autowired
-    Book book;
+    private Book book;
 
     @GetMapping("/book")
     public String book() {
         System.out.println("--------" + book.toString());
         return book.toString();
+    }
+
+    // map 作为入参
+    @RequestMapping("/testmapparam")
+    public Map<String,Object> book(@RequestBody Map<String,Object> paramMap) {
+        System.out.println("--------" + JSON.toJSONString(paramMap.get("param")));
+        return paramMap;
     }
 }
